5ace291a63922adfdd7b1090ae885fd00f959d84
Данил-Каширский <kashirskiy75@bk.ru>
2023-09-06 18:09:09 +0700

    Creating first site list

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ app = Flask (__name__)
 
 @app.route ("/")
 def start():
-    return "web-server in flask"
+    return """
+<!doctype HTML>
+
+<html>
+
+    <head>
+        <title>Kashirskiy Danil Sergeevich, Lab.work №1</title>
+    </head>
+
+    <body>
+        <header>
+            NSTU, FB, Laboratory work №1
+        </header>
+
+        <h1>WEB-server in flask</h1>
+
+        <footer>
+            &copy; Kashirskiy Danil, FBI-14, III course, 2023
+        </footer>
+    </body>
+</html>  
+"""

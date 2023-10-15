605550d39b075ee67853b38262f9389d8bc97c43
Каширский Данил <kashirskiy75@bk.ru>
2023-10-16 01:59:06 +0700

    Routs

--- app.py
+++ app.py
@@ -25,8 +25,10 @@ def lab1():
         программирования Python, использующий набор инструментов
         Werkzeug,  также шаблонизатор Jinja2. Относится к категории так
         называемых микрофреймворков — минималистичных каркасов
-        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности. 
         </p>
+
+        <li><a href="menu" target="_blank" >Laboratory menu</a></li>
 
         <footer>
             &copy; Kashirskiy Danil, FBI-14, III course, 2023
@@ -56,6 +58,18 @@ def menu():
         <ol>
             <li>
                 <a href="lab1" target="_blank" >Laboratory work #1</a>
+            </li>
+            <li>
+                <a href="lab1/lab1pic" target="_blank">Laboratory work #1(good tree)</a>
+            </li>
+            <li>
+                <a href="lab1/student" target="_blank">Laboratory work #1(student)</a>
+            </li>
+            <li>
+                <a href="lab1/pythome" target="_blank">Laboratory work #1(python)</a>
+            </li>
+            <li>
+                <a href="lab1/yamaguchi" target="_blank">Laboratory work #1(Yamaguchi)</a>
             </li>
             <li>
                 <a href="lab2" target="_blank" >Laboratory work #2</a>
@@ -120,3 +134,141 @@ def lab1pic():
 </html>  
 '''
 
+@app.route ("/lab1/student")
+def lab1student():
+    return '''
+<!doctype html>
+
+<html>
+
+    <head>
+        <title>Kashirskiy Danil Sergeevich, Lab.work №1</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+
+    <body>
+
+        <header>
+            NSTU, FB, Laboratory work №1
+        </header>
+
+        <h1>Kashirskiy Danil Sergeevich</h1>
+
+        <img src="''' + url_for('static', filename='lab1nstu.jpg') + '''" style="margin: 10px auto 20px; display: block; width: 45%">
+        
+        <footer>
+            &copy; Kashirskiy Danil, FBI-14, III course, 2023
+        </footer>
+
+    </body>
+</html>  
+'''
+
+@app.route ("/lab1/pythome")
+def pythome():
+    return '''
+<!doctype html>
+
+<html>
+
+    <head>
+        <title>Kashirskiy Danil Sergeevich, Lab.work №1</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+
+    <body>
+
+        <header>
+            NSTU, FB, Laboratory work №1
+        </header>
+
+        <h1>Что такое Python ?</h1>
+
+        <p>
+        Python — это язык программирования, который широко используется в интернет-приложениях, 
+        разработке программного обеспечения, науке о данных и машинном обучении (ML). 
+        Разработчики используют Python, потому что он эффективен, 
+        прост в изучении и работает на разных платформах. Программы на языке Python можно скачать бесплатно, 
+        они совместимы со всеми типами систем и повышают скорость разработки.
+        </p>
+
+        <img src="''' + url_for('static', filename='piton.png') + '''" style="margin: 10px auto 20px; display: block; width: 20%">
+
+        <h2>Где применяется Python ?</h2>
+
+        <p>
+        Веб-разработка на стороне сервера включает в себя сложные серверные функции, 
+        с помощью которых веб-сайты отображают информацию для пользователя. 
+        Например, веб-сайты должны взаимодействовать с базами данных и другими веб-сайтами, а также защищать данные при их отправке по сети. 
+
+        Python полезен при написании серверного кода, поскольку он предлагает множество библиотек, 
+        состоящих из предварительно написанного кода для сложных серверных функций. 
+        Также разработчики используют широкий спектр платформ Python, 
+        которые предоставляют все необходимые инструменты для более быстрого и простого создания интернет-приложений. 
+        Например, разработчики могут создать «скелет» интернет-приложения за считанные секунды, потому что им не нужно писать код с нуля. 
+        Затем его можно протестировать с помощью инструментов тестирования платформы независимо от внешних инструментов тестирования.
+        </p>
+
+
+        <footer>
+            &copy; Kashirskiy Danil, FBI-14, III course, 2023
+        </footer>
+
+    </body>
+</html>  
+'''
+
+@app.route ("/lab1/yamaguchi")
+def yamaguchi():
+    return '''
+<!doctype html>
+
+<html>
+
+    <head>
+        <title>Kashirskiy Danil Sergeevich, Lab.work №1</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+
+    <body>
+
+        <header>
+            NSTU, FB, Laboratory work №1
+        </header>
+
+        <h1>Современное японское оборудование от Yamaguchi</h1>
+
+        <p>
+        YAMAGUCHI — одна из лидирующих компаний в мире, которая задаёт тренды в области массажного оборудования, 
+        фитнес-тренажеров, товаров для красоты и здоровья! Мы являемся официальными дистрибьюторами известных мировых производителей, 
+        среди которых Yamaguchi, US Medica, Fujiiryoki, Hakuju, Bestec и Anatomico.
+        
+        Миссия компании Yamaguchi — прививать любовь к здоровью! 
+        Мы заботимся о качестве жизней наших клиентов, разрабатывая продукцию по последним 
+        техническим и дизайнерским новшествам совместно с врачами-мануальными терапевтами, 
+        врачами-косметологами и профессиональными фитнес-тренерами. 
+        Каждая новинка перед выходом проходит строгий контроль качества и многократно тестируется, что подтверждает 
+        безопасность и надежность. Наше оборудование позволяет 
+        моментально восстановить энергию и силы, привести фигуру в форму, 
+        поддержать идеальный вес, укрепить здоровье и позаботиться о красоте. В совокупности 
+        со стильным дизайном и передовыми японскими технологиями каждый продукт — настоящий шедевр!
+        </p>
+
+        <img src="''' + url_for('static', filename='kreslo.jpg') + '''" style="margin: 10px auto 20px; display: block; width: 45%">
+
+        <p>
+        Наша компания объединяет Россию здоровьем! В салонах Yamaguchi от Калининграда до Владивостока можно познакомиться с брендом, 
+        протестировать продукцию и получить профессиональную консультацию от менеджеров. 
+        Доставку мы осуществляем через собственную логистическую службу в течение 24-х часов по всей России. 
+        Сотрудники логистики — профессионалы, которые не только доставляют оборудование в нужное помещение, 
+        но и подробно рассказывают о товаре и обучают им пользоваться.
+        </p>
+
+
+        <footer>
+            &copy; Kashirskiy Danil, FBI-14, III course, 2023
+        </footer>
+
+    </body>
+</html>  
+'''

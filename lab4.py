0c7e645085a92e432c001f27313fd33c79ca0272
Данил Каширский <kashirskiy75@bk.ru>
2023-10-18 18:58:09 +0700

    Форма авторизации(доделать)

--- lab4.py
+++ lab4.py
@@ -4,3 +4,8 @@ lab4 = Blueprint ('lab4', __name__)
 @lab4.route ('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route ('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')  

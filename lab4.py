bb53ab295d114f3d578e8bcdd1edabca62925840
Каширский Данил <kashirskiy75@bk.ru>
2023-10-31 16:50:15 +0700

    авторизация

--- lab4.py
+++ lab4.py
@@ -8,4 +8,16 @@ def lab():
 
 @lab4.route ('/lab4/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')  
+    if request.method == 'GET':
+        return render_template('login.html') 
+
+
+    username = request.form.get ('username')
+    password = request.form.get ('password')
+    if username == 'alex' and password == '123':
+        return render_template('success_lab4.html')
+
+
+    if username == '' or username != 'alex' or password == '' or password != '123':
+        error = 'Неверный логин и/или пароль'
+        return render_template ('login.html', error = error)

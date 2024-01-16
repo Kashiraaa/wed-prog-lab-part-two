d8b4de0a9bd8e526de123a78924e9036920cd9ed
Каширский Данил <kashirskiy75@bk.ru>
2024-01-17 02:43:26 +0700

    Редактирование выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -32,3 +32,12 @@ def del_course(course_num):
         abort(404)  
     del courses[course_num]
     return '', 204
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_course(course_num):
+    course = request.get_json()
+    if course_num < 0 or course_num >= len(courses):
+        abort(404) 
+    course["created_date"] = datetime.now()
+    courses[course_num] = course
+    return courses[course_num]

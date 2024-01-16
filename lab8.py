73b7957b79ecdbec68f56264ca09126c83ff44dc
Каширский Данил <kashirskiy75@bk.ru>
2024-01-17 02:41:02 +0700

    Получение информации о курсе/-ах по REST

--- lab8.py
+++ lab8.py
@@ -7,3 +7,21 @@ lab8 = Blueprint('lab8',__name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000, "created_date": datetime.now()},
+    {"name": "basic", "videos": 30, "price": 450, "created_date": datetime.now()},
+    {"name": "c#", "videos": 8, "created_date": datetime.now()}
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=['GET']) 
+def get_courses():
+    return jsonify(courses)
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)  
+    return courses[course_num]

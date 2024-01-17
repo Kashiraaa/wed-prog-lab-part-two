c4833e6896d3892d5502effb9460278290c9690f
Каширский Данил <kashirskiy75@bk.ru>
2024-01-17 16:57:28 +0700

    Карточка  с поздравлением

--- lab9.py
+++ lab9.py
@@ -4,7 +4,23 @@ lab9 = Blueprint('lab9',__name__)
 
 @lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+    if request.method == 'POST':
+        sender_name = request.form.get('sender_name')
+        recipient_gender = request.form.get('recipient_gender')
+        recipient_name = request.form.get('recipient_name')
+
+        return redirect(url_for('lab9.card', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name))
+    else:
+        return render_template('lab9/index.html')
+
+
+@lab9.route('/lab9/happen', methods=['GET'])
+def card():
+    sender_name = request.args.get('sender_name')
+    recipient_gender = request.args.get('recipient_gender')
+    recipient_name = request.args.get('recipient_name')
+
+    return render_template('lab9/happen.html', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name)
 
 @lab9.app_errorhandler(404)
 def not_found(e):
